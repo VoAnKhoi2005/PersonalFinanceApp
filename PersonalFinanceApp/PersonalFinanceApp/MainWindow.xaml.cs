44b66d98c351c2e1446e3230ba9cd408155d2fba
kine1606 <ntkien160605@gmail.com>
2024-10-27 01:33:12 +0700

    LoginWindow
    add LoginWindow + some patterns from MaterialDeignInXAML

--- PersonalFinanceApp/PersonalFinanceApp/MainWindow.xaml.cs
+++ PersonalFinanceApp/PersonalFinanceApp/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace PersonalFinanceApp
         public MainWindow()
         {
             InitializeComponent();
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
         }
     }
 }
