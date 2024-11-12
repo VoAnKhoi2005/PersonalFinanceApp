471aa7a3863475add99824701583787dafe635b5
Khoi Vo <23520790@gm.uit.edu.vn>
2024-11-12 07:59:25 +0700

    Add the reset dynamic view switch

--- PersonalFinanceApp/PersonalFinanceApp/src/ViewModel/Command/ConfirmEmailCommand.cs
+++ PersonalFinanceApp/PersonalFinanceApp/src/ViewModel/Command/ConfirmEmailCommand.cs
@@ -3,17 +3,21 @@ using PersonalFinanceApp.ViewModel.Stores;
 
 namespace PersonalFinanceApp.ViewModel.Command;
 
-public class ConfirmCommand : BaseCommand
+public class ConfirmEmailCommand : BaseCommand
 {
     private readonly LoginNavigationStore _navigationStore;
+    private readonly ResetPasswordModelView resetPasswordVM;
 
-    public ConfirmCommand(LoginNavigationStore navigationStore)
+    public ConfirmEmailCommand(LoginNavigationStore navigationStore, ResetPasswordModelView resetPasswordModelView)
     {
         _navigationStore = navigationStore;
+        resetPasswordVM = resetPasswordModelView;
     }
 
     public override void Execute(object parameter)
     {
         _navigationStore.CurrentViewModel = new CodeVerificationModelView(_navigationStore);
     }
+
+    public override bool CanExecute(object parameter) => resetPasswordVM.VerifyEmail();
 }
