ecbed98c63b94b391398b9eb2aaf728269f6f5cc
Steve Sephen <honguyentailoi05@gmail.com>
2024-11-20 22:23:16 +0700

    add class expensebook

---
+++ PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/MainMenu/ExpenseBookAddNewViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PersonalFinanceApp.ViewModel.Command;
+using PersonalFinanceApp.ViewModel.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PersonalFinanceApp.Src.ViewModel.MainMenu; 
+internal class ExpenseBookAddNewViewModel {
+    private readonly ModalNavigationStore _modalNavigationStore;
+
+    public ICommand ExpenseBookCancelCommand { get; set; }
+
+    public ExpenseBookAddNewViewModel(IServiceProvider serviceProvider) {
+        _modalNavigationStore = serviceProvider.GetRequiredService<ModalNavigationStore>();
+        ExpenseBookCancelCommand = new RelayCommand<object>(CloseModal);
+    }
+
+    private void CloseModal(object sender) {
+        _modalNavigationStore.Close();
+    }
+}
---
+++ PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/MainMenu/ExpenseBookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PersonalFinanceApp.Database;
+using PersonalFinanceApp.Model;
+using PersonalFinanceApp.ViewModel.Command;
+using PersonalFinanceApp.ViewModel.MainMenu;
+using PersonalFinanceApp.ViewModel.Stores;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PersonalFinanceApp.Src.ViewModel.MainMenu; 
+internal class ExpenseBookViewModel {
+    private readonly ModalNavigationStore _modalNavigationStore;
+    public ICommand AddNewExpenseBookCommand { get; set; }
+
+    public ExpenseBookViewModel(IServiceProvider serviceProvider) {
+        _modalNavigationStore = serviceProvider.GetRequiredService<ModalNavigationStore>();
+        //AddNewExpenseBookCommand = new NavigateModalCommand<ExpenseBookAddNewViewModel>(serviceProvider);
+    }
+}
