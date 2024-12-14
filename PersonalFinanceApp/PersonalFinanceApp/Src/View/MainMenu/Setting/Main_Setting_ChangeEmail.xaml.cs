6cbf3d1d6f121d694e7e046bfb39a9490b1d193d
kine1606 <ntkien160605@gmail.com>
2024-12-14 16:28:01 +0700

    add setting change : change password, change gmail
    pop up user
    pop up notification

---
+++ PersonalFinanceApp/PersonalFinanceApp/Src/View/MainMenu/Setting/Main_Setting_ChangeEmail.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonalFinanceApp.Src.View.MainMenu.Setting
+{
+    /// <summary>
+    /// Interaction logic for Main_Setting_ChangeEmail.xaml
+    /// </summary>
+    public partial class Main_Setting_ChangeEmail : UserControl
+    {
+        public Main_Setting_ChangeEmail()
+        {
+            InitializeComponent();
+        }
+    }
+}
