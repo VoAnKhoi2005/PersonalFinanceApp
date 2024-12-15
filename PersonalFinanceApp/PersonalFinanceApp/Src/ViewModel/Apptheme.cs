5b50b00817d621e1d570333aeeace5ec589b510f
kine1606 <ntkien160605@gmail.com>
2024-12-15 12:10:29 +0700

    change color, fix all UI presenter

---
+++ PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/Apptheme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PersonalFinanceApp.Src.ViewModel
+{
+    class Apptheme
+    {
+        public static void ChangeTheme(Uri themeUri)
+        {
+            ResourceDictionary Theme = new ResourceDictionary() { Source = themeUri };
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(Theme);
+        }
+    }
+}
--- PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/MainMenu/Setting/SettingViewModel.cs
+++ PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/MainMenu/Setting/SettingViewModel.cs
@@ -104,25 +104,25 @@ public class SettingViewModel : BaseViewModel {
         }
         return usr.VerifyPassword(passwrd);
     }
-    public void ExportToExcel(DataGrid dgrid) {
-        Excel.Application excel = new Excel.Application();
-        excel.Visible = true;
-        Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
-        Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+    //public void ExportToExcel(DataGrid dgrid) {
+    //    Excel.Application excel = new Excel.Application();
+    //    excel.Visible = true;
+    //    Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+    //    Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
 
-        for (int j = 0; j < dgrid.Columns.Count; j++) 
-        {
-            Range myRange = (Range)sheet1.Cells[1, j + 1];
-            sheet1.Cells[1, j + 1].Font.Bold = true; 
-            sheet1.Columns[j + 1].ColumnWidth = 15; 
-            myRange.Value2 = dgrid.Columns[j].Header;
-        }
-        for (int i = 0; i < dgrid.Columns.Count; i++) { 
-            for (int j = 0; j < dgrid.Items.Count; j++) {
-                TextBlock b = dgrid.Columns[i].GetCellContent(dgrid.Items[j]) as TextBlock;
-                Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
-                myRange.Value2 = b.Text;
-            }
-        }
-    }
+    //    for (int j = 0; j < dgrid.Columns.Count; j++) 
+    //    {
+    //        Range myRange = (Range)sheet1.Cells[1, j + 1];
+    //        sheet1.Cells[1, j + 1].Font.Bold = true; 
+    //        sheet1.Columns[j + 1].ColumnWidth = 15; 
+    //        myRange.Value2 = dgrid.Columns[j].Header;
+    //    }
+    //    for (int i = 0; i < dgrid.Columns.Count; i++) { 
+    //        for (int j = 0; j < dgrid.Items.Count; j++) {
+    //            TextBlock b = dgrid.Columns[i].GetCellContent(dgrid.Items[j]) as TextBlock;
+    //            Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+    //            myRange.Value2 = b.Text;
+    //        }
+    //    }
+    //}
 }
