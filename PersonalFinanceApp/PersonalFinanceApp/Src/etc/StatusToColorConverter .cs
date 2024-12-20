91e5e8b961e702fb8851f88ff736a07a1195c63c
kine1606 <ntkien160605@gmail.com>
2024-12-20 17:31:36 +0700

    small fix p2
    ahihi

--- PersonalFinanceApp/PersonalFinanceApp/Src/etc/StatusToColorConverter .cs
+++ PersonalFinanceApp/PersonalFinanceApp/Src/etc/StatusToColorConverter .cs
@@ -4,12 +4,15 @@ using System.Windows.Media;
 
 namespace PersonalFinanceApp.Src.etc;
 public class StatusToColorConverter : IValueConverter {
+    public SolidColorBrush color1 = new SolidColorBrush(Color.FromRgb(21, 249, 17));
+    public SolidColorBrush color2 = new SolidColorBrush(Color.FromRgb(223, 58, 32));
+    public SolidColorBrush color3 = new SolidColorBrush(Color.FromRgb(182, 54, 220));
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
         if (value is string status) {
             return status switch {
-                "Completed" => Brushes.Green,
-                "Canceled" => Brushes.Red,
-                "Active" => Brushes.Orange,
+                "Completed" => color1,
+                "Canceled" => color2,
+                "Active" => color3,
                 _ => Brushes.Black
             };
         }
