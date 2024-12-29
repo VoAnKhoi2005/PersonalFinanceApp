789964d8418c5e748917b5450cd72ad4564e4530
Steve Sephen <honguyentailoi05@gmail.com>
2024-12-29 23:10:23 +0700

    small fix

--- PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/Stores/AccountStore.cs
+++ PersonalFinanceApp/PersonalFinanceApp/Src/ViewModel/Stores/AccountStore.cs
@@ -1,4 +1,5 @@
-﻿using PersonalFinanceApp.Model;
+﻿using PersonalFinanceApp.Database;
+using PersonalFinanceApp.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,6 +7,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PersonalFinanceApp.Src.ViewModel.Stores;
 public class AccountStore : INotifyPropertyChanged {
@@ -29,6 +31,33 @@ public class AccountStore : INotifyPropertyChanged {
                 _usersID = value;
                 OnPropertyChanged(nameof(_usersID));
             }
+        }
+    }
+    public event Action TriggerActionUploadSaving;
+    public void UploadSaving() {
+        TriggerActionUploadSaving = LoadSaving;
+        TriggerActionUploadSaving?.Invoke();
+    }
+    public void LoadSaving() {
+        //load saving in user
+        try {
+            long saving = 0;
+            var items = DBManager.GetCondition<ExpensesBook>(e => e.UserID == int.Parse(UsersID));
+            foreach (var item in items) {
+                item.Expenses = DBManager.GetCondition<Expense>(e => e.UserID == int.Parse(UsersID) && item.Month == e.ExBMonth && item.Year == e.ExBYear);
+                saving += item.Budget - item.Expenses.Sum(ex => ex.Amount);
+            }
+            var itemgoal = DBManager.GetCondition<Goal>(g => g.UserID == Users.UserID);
+            foreach (var item in itemgoal) {
+                saving += item.CurrentAmount;
+            }
+            var usr = DBManager.GetFirst<User>(u => u.UserID == int.Parse(UsersID));
+            if (usr != null) { usr.Saving = saving; }
+            DBManager.Update(usr);
+            Users.Saving = saving;
+        }
+        catch (Exception ex) {
+            MessageBox.Show("Có lỗi xảy ra vui lòng thử lại", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
     public event PropertyChangedEventHandler? PropertyChanged;
